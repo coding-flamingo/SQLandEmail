569527b80f731f90094db158bd40cf78f819530f
igalfsg <igalfsg@gmail.com>
2020-01-18 14:23:49 -0800

    added loading and input validation

--- SQLandEmailwithBlazorPage/SQLandEmailwithBlazorPage/Models/PersonModel.cs
+++ SQLandEmailwithBlazorPage/SQLandEmailwithBlazorPage/Models/PersonModel.cs
@@ -10,12 +10,14 @@ namespace SQLandEmailwithBlazorPage.Models
     {
         [Key]
         [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "First Name is required")]
         public string FirstName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Last Name is required")]
         public string LastName { get; set; }
         [Required]
+        [Range(1, 99, ErrorMessage = "Please enter your real age.")]
         public int Age { get; set; }
         public string Ocupation { get; set; }
         public string Hobbies { get; set; }
